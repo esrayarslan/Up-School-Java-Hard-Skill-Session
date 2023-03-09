ade8454f179bad468ccc1677c9ad2cdca7e394be
Esra Arslan <esrayildizarslan@gmail.com>
2023-03-09 15:40:56 +0300

    question + solution

--- src/hardSkillSession10/Example.java
+++ src/hardSkillSession10/Example.java
@@ -1,10 +1,27 @@
 package hardSkillSession10;
 
+import java.util.Scanner;
+
 public class Example {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		//The Java code that finds the largest of the 5 numbers entered from the keyboard
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int max = Integer.MIN_VALUE;
+		
+		for(int i = 1; i <= 5; i++) {
+			System.out.print(i + " .enter the number: ");
+			int num = scanner.nextInt();
+			
+			if(num > max) 
+				max = num;
+				
+			 } 
+		System.out.print(max);
+		
 	}
 
 }
