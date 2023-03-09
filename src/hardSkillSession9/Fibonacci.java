6ffea653643101ab046fa1e08ca8bc65e9066abc
Esra Arslan <esrayildizarslan@gmail.com>
2023-03-09 14:08:24 +0300

    the second solution

--- src/hardSkillSession9/Fibonacci.java
+++ src/hardSkillSession9/Fibonacci.java
@@ -30,7 +30,35 @@ public class Fibonacci {
 			
 			//When we do it with the Fibonacci method
 			
-			
+			/*
+			 * import java.util.Scanner;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		
+
+		Scanner input = new Scanner(System.in);
+		System.out.print(" Enter a number from the keyboard : ");
+		int n = input.nextInt();
+		
+		if(n == 0) {
+			System.out.println("There is no fibonacci number to calculate...");
+		} else {
+			for(int i = 0; i<n; i++){
+				System.out.print(fibonacci(i) + " ");
+				
+		}
+		
+		private static int fibonacci (int i){
+				if (i == 0 || i ==1)
+				return =1;
+				
+				else 
+				return fibonacci (i -1) + fibonacci (i -2);
+				
+				}
+			 */
 		}
 	}
 
