0307126f6d4cde856d4e9110ce9467aec54de835
Esra Arslan <esrayildizarslan@gmail.com>
2023-03-09 22:40:56 +0300

    question + solution

---
+++ src/hardSkillSession10/Example5.java
@@ -0,0 +1,27 @@
+package hardSkillSession10;
+
+import java.util.Scanner;
+public class Example5 {
+
+	public static void main(String[] args) {
+		// Java code that finds the factorial of the number entered from the keyboard (using recursive methods)
+		Scanner inp = new Scanner(System.in);
+		
+		System.out.print("Enter the number you want to find the factoring :");
+		int number = inp.nextInt();
+		
+		System.out.println(calculateFactorial(number));
+		
+		
+	}
+	
+	public static int calculateFactorial(int number) {
+		if (number == 0) {
+			return 1;
+		} else {
+			return number * calculateFactorial(number - 1);
+			
+		}
+	}
+}
+			
