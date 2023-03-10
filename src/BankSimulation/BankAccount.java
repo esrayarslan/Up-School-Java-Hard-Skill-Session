15f7995ce52e8538bf6483ddc6c396aa62991126
Esra Arslan <esrayildizarslan@gmail.com>
2023-03-10 22:00:49 +0300

    question + answer

--- src/BankSimulation/BankAccount.java
+++ src/BankSimulation/BankAccount.java
@@ -2,40 +2,44 @@ package BankSimulation;
 
 public class BankAccount {
 
-	String accountNumber;
-	String ownerFirstName;
-	String ownerLastName;
-	double balance;
-	
-	public BankAccount(String accountNumber,String ownerFirstName,String ownerLastName,double balance) {
-		
-	this.accountNumber = accountNumber;
-	this.ownerFirstName = ownerFirstName;
-	this.ownerLastName = ownerLastName;
-	this.balance = balance;
-	
-	}
-	
-	public void deposit(double amount) {
-		balance += amount;
-	}
-	
-	public void withdraw(double amount) {
-		if(amount > balance) {
-			System.out.println("There is not enough money in your account!");
-			return;
-	}
-		if(amount < 0) {
-			System.out.println("Invalid Amount!");
-			return;
-	}
-		balance -= amount;
-		
-	}
-	
-	public void displayBalance() {
-		System.out.println("Your Account Balance : " + balance);
-	}
-	
+	  String accountNumber;
 
-}
+	  String ownerFirstName;
+
+	  String ownerLastName;
+
+	  double balance;
+
+	  //constructor  -> kurucu metodumuz
+	  // class ismi ile aynı olmak zorunda
+	  // dönüş tipi yoktur
+	  // class'ların örnekleri oluşturulduğunda ilk çalışan metodtur
+	  public BankAccount(String accountNumber, String ownerFirstName
+	      , String ownerLastName, double balance) {
+	    this.accountNumber = accountNumber;
+	    this.ownerFirstName = ownerFirstName;
+	    this.ownerLastName = ownerLastName;
+	    this.balance = balance;
+	  }
+
+	  public void deposit(double amount) {
+	    balance += amount;
+	  }
+
+	  public boolean withdraw(double amount) {
+	    if (amount > balance) {
+	      System.out.println("Hesabınızda yeterli para yok!");
+	      return false;
+	    }
+	    if (amount < 0) {
+	      System.out.println("Geçersiz tutar");
+	      return false;
+	    }
+	    balance -= amount;
+	    return true;
+	  }
+
+	  public void displayBalance() {
+	    System.out.println("Hesap bakiyeniz: " + balance);
+	  }
+	}
--- src/BankSimulation/Main.java
+++ src/BankSimulation/Main.java
@@ -4,99 +4,89 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in); 
-		
-		BankAccount[] accounts = new BankAccount[100];
-		//var newAccountarry = new BankAccount[account.length + 1];
-		
-		int numAccounts = 0;
-		boolean quit = false;
-		
-		do {
-			 System.out.print("1. Create a New Account");
-			 System.out.print("2. Deposit Money to the Account");
-			 System.out.print("3. Withdraw Money from the Account");
-			 System.out.print("4. View Account Balance");
-			 System.out.print("5. Exit");
-			 System.out.print("6. Your choice (1-5) : ");
-			 int choice = sc.nextInt();
-			 
-			 switch (choice) {
-			 case 1 -> {
-				 System.out.print("Account Number");
-				 String accountNumber = sc.nextLine();
-				 
-				 System.out.print("Your Name : ");
-				 String firstName = sc.nextLine();
-				 
-				 System.out.print("Your Last Name : ");
-				 String lastName = sc.nextLine();
-				 
-				 System.out.print("First deposit balances :");
-				 double initialBalance = sc.nextDouble();
-				 
-				 BankAccount newBankAccount = new BankAccount(accountNumber,
-						 										firstName,
-						 										lastName,
-						 										initialBalance);
-				 accounts[numAccounts] = newBankAccount;
-				 numAccounts++;
-				 System.out.print("A New Account has Been Created !");
-				 
-				 }
-			 
-			 case 2 -> {
-				 System.out.print("Account Number : ");
-				 String accountNumber = sc.nextLine();
-				 
-				 System.out.print("Amount to Be Deposited : ");
-				 double amountDeposit = sc.nextDouble();
-				 
-				 for (int i = 0; i < numAccounts; i++) {
-					 if (accounts[i].accountNumber == accountNumber) {
-						 accounts[i].deposit(amountDeposit);
-						 System.out.print(amountDeposit + "Money has Been Deposited to The Account");
-						 break;
-					 }
-				 }
-			 }
-			  
-			 case 3 -> {
-				 System.out.print("Account Number : ");
-				 String accountNumber = sc.nextLine();
-				 
-				 System.out.print("The Amount to Be Withdrawn : ");
-				 double withdrawAmount = sc.nextDouble();
-				 
-				 for (int i = 0; i < numAccounts; i++) {
-					 if (accounts[i].accountNumber == accountNumber) {
-						 accounts[i].withdraw(withdrawAmount);
-						 System.out.print(withdrawAmount + "Money Was Withdrawn from the Account");
-						 break;
-					 }
-				 }
-			 }
-			 
-			 case 4 -> {
-				 System.out.print("Account Number : ");
-				 String accountNumber = sc.nextLine();
+	  public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    BankAccount[] accounts = new BankAccount[100];
+	    //var newAccountArray = new BankAccount[accounts.length + 1];
+	    int numAccounts = 0;
+	    boolean quit = false;
+	    do {
+	      System.out.println("1. Yeni hesap oluştur");
+	      System.out.println("2. Hesaba para yatır");
+	      System.out.println("3. Hesaptan para çek");
+	      System.out.println("4. Hesap bakiyesini görüntüle");
+	      System.out.println("5. Çıkış");
+	      System.out.println("Seçiminiz (1-5): ");
+	      int choice = sc.nextInt();
+	      switch (choice) {
+	        case 1 -> {
+	          System.out.println("Hesap numarası: ");
+	          String accountNumber = sc.next();
+	          System.out.println("Adınız: ");
+	          String firstName = sc.next();
+	          System.out.println("Soyadınız: ");
+	          String lastName = sc.next();
+	          System.out.println("İlk para yatırma bakiyesi: ");
+	          double initialBalance = sc.nextDouble();
+	          BankAccount newBankAccount = new BankAccount(accountNumber,
+	                                                       firstName,
+	                                                       lastName,
+	                                                       initialBalance);
+	          accounts[numAccounts] = newBankAccount;
+	          numAccounts++;
+	          System.out.println("Yeni hesap oluşturuldu.");
+	        }
+	        case 2 -> {
+	          System.out.print("Hesap numarası: ");
+	          String accountNumber = sc.next();
+	          System.out.println("Yatırılacak miktar: ");
+	          double amountDeposit = sc.nextDouble();
+	          for (int i = 0; i < numAccounts; i++) {
+	            // accounts[i].accountNumber.equals(accountNumber)
+	            if (accounts[i].accountNumber.equals(accountNumber)) {
+	              accounts[i].deposit(amountDeposit);
+	              System.out.println(amountDeposit + "₺ hesaba yatırıldı.");
+	              break;
+	            }
+	          }
+	        }
+	        case 3 -> {
+	          System.out.println("Hesap numarası: ");
+	          String accountNumber = sc.next();
+	          System.out.println("Çekilecek miktar: ");
+	          double withdrawAmount = sc.nextDouble();
+	          for (int i = 0; i < numAccounts; i++) {
+	            // accounts[i].accountNumber.equals(accountNumber)
+	            if (accounts[i].accountNumber.equals(accountNumber)) {
+	              boolean result = accounts[i].withdraw(withdrawAmount);
+	              if (result) {
+	                System.out.println(withdrawAmount + "₺ çekildi.");
+	              }
+	              break;
+	            }
+	          }
+	        }
+	        case 4 -> {
+	          System.out.println("Hesap numarası: ");
+	          String accountNumber = sc.next();
+	          for (int i = 0; i < numAccounts; i++) {
 
-				 for (int i = 0; i < numAccounts; i++) {
-					 if (accounts[i].accountNumber == accountNumber) {
-						 accounts[i].displayBalance();
-						 System.out.print(withdrawAmount + "Money Was Withdrawn from the Account");
-						 break;
-					 }
-				 }
-			 }
-				 
-			 }
-			 
-			 }
-			 }while (!quit);
+	        	  System.out.println(
+	  	                accounts[i].accountNumber + "==" + accountNumber + "=" + accounts[i].accountNumber == accountNumber);
+	            System.out.println(
+	                accounts[i].accountNumber + "==" + accountNumber + "=" + accounts[i].accountNumber.equals(
+	                    accountNumber));
 
+	            if (accounts[i].accountNumber.equals(accountNumber)) {
+	              accounts[i].displayBalance();
+	              break;
+	            }
+	          }
+	        }
+	        case 5 -> quit = true;
+	      }
+	    } while (!quit);
+
+	    System.out.println("--------------------");
+	  }
 	}
-
-}
