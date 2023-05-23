3cc9acc3b0c07c0c9a18366b9243f112bc04c3c8
esrayarslan <esrayildizarslan@gmail.com>
2023-05-23 16:01:51 +0300

    rent a car simulation

---
+++ src/RentACarSimulation/DomainConstants.java
@@ -0,0 +1,8 @@
+package RentACarSimulation;
+
+public class DomainConstants {
+	
+	public static final int MINIMUM_TRUCK_RENT_DAY_COUNT = 10;
+	  public static final int MINIMUM_CAR_RENT_DAY_COUNT = 5;
+
+}
