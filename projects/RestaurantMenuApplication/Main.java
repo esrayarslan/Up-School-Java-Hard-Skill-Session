466ed5913c2f7e64aba0b6154084bc2fd1fbb5a3
esrayarslan <esrayildizarslan@gmail.com>
2023-05-24 18:10:58 +0300

    restaurant menu app

--- projects/RestaurantMenuApplication/Main.java
+++ projects/RestaurantMenuApplication/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 		
 		public static void main(String[] args) {
-	        Meal meal = new Meal("Mercimek Çorbası",12.5,new String[]{"mercime,yağ,su,tuz"},100,true,true);
+	        Meal meal = new Meal("Mercimek Çorbası",12.5,new String[]{"mercimek,yağ,su,tuz"},100,true,true);
 	        Meal meal2 = new Meal("Yaprak Sarma",5,new String[]{"prinç,salça"},200,true,false);
 	        Meal meal3 = new Meal("Pizza",50,new String[]{"un,süt,sucuk,peynir"},500,false,false);
 	        Meal meal4 = new Meal("Köfte",12.5,new String[]{"kıyma,baharat"},1000,false,true);
