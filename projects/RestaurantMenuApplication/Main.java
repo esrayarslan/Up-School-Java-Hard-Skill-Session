1077341a419b8b514509a76f2c9d4e87a19cdf7a
esrayarslan <esrayildizarslan@gmail.com>
2023-05-24 18:05:14 +0300

    restaurant menu app

--- projects/RestaurantMenuApplication/Main.java
+++ projects/RestaurantMenuApplication/Main.java
@@ -2,8 +2,25 @@ package RestaurantMenuApplication;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		public static void main(String[] args) {
+	        Meal meal = new Meal("Mercimek Çorbası",12.5,new String[]{"mercime,yağ,su,tuz"},100,true,true);
+	        Meal meal2 = new Meal("Yaprak Sarma",5,new String[]{"prinç,salça"},200,true,false);
+	        Meal meal3 = new Meal("Pizza",50,new String[]{"un,süt,sucuk,peynir"},500,false,false);
+	        Meal meal4 = new Meal("Köfte",12.5,new String[]{"kıyma,baharat"},1000,false,true);
+
+	        Menu menu = new Menu();
+	        menu.addMeal(meal);
+	        menu.addMeal(meal3);
+	        menu.addMeal(meal4);
+	        menu.addMeal(meal3);
+	        menu.addMeal(meal2);
+
+
+	        menu.removeMeal(meal2);
+
+	        menu.printAllMeals();
+	    
 
 	}
 
--- projects/RestaurantMenuApplication/Menu.java
+++ projects/RestaurantMenuApplication/Menu.java
@@ -1,5 +1,69 @@
 package RestaurantMenuApplication;
 
-public class Menu {
+import java.util.Arrays;
 
+public class Menu {
+	
+	private Meal[] meals = new Meal[10];
+	private int mealCount = 0;
+	
+	public void addMeal(Meal meal) {
+		
+		meals[mealCount] = meal;
+		mealCount++;
+		
+ 	}
+	
+	public void removeMeal(Meal meal) {
+		
+		int index = -1;
+		for(int i = 0; i < mealCount; i++) {
+			index = i;
+			break;
+		}
+	
+ 
+	if(index != -1) {
+		for(int i = index; i < mealCount; i++) {
+			meals[i] = meals[i + 1];
+		}
+		meals[mealCount -1] = null;
+		mealCount--;
+	} else
+		System.out.print("Food could not be found");
 }
+	public void printAllMeals() {
+		System.out.println("-- MENU --");
+		
+		for(int i = 0; i < mealCount; i++) {
+			Meal meal = meals[i];
+			
+			System.out.println("Food Name : " + meal.getName());
+            System.out.println("Amount : " + meal.getPrice());
+            System.out.println("Materials : " + Arrays.toString(meal.getIngredients()));
+            System.out.println("Calories : " + meal.getCalories());
+            System.out.println("Vegan : " + (meal.isVegan() ? "YES" : "NO"));
+            System.out.println("Does it contain gluten : " + (meal.isGlutenFree() ? "YES" : "NO"));
+            System.out.println("----------------------------");
+        }
+        System.out.println("Total Price :" + totalPrice());
+        System.out.println("Total Calories:" + totalCalories());
+    }
+	
+	private double totalPrice() {
+        double toplam = 0;
+        for (int i = 0 ; i<mealCount ; i++){
+            toplam += meals[i].getPrice();
+        }
+        return toplam;
+    }
+    private double totalCalories() {
+        double toplam = 0;
+        for (int i = 0 ; i<mealCount ; i++){
+            toplam += meals[i].getCalories();
+        }
+        return toplam;
+    }
+}
+		
+	
