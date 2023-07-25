6c8b335381fed58b8ec45098ca4f1f978111f3d3
esrayarslan <esrayildizarslan@gmail.com>
2023-07-25 15:27:37 +0300

    added

--- projects/HardSkillSession19/Main.java
+++ projects/HardSkillSession19/Main.java
@@ -1,5 +1,26 @@
 package HardSkillSession19;
 
 public class Main {
+	public static void main(String[] args) {
+		Shape shape = new Shape(1, 2);
+		var area = shape.area();
+		var perimeter = shape.perimeter();
+		System.out.println("Area : " + area + " perimetir : " + perimeter);
+		
+		Shape shapeRectangle = new Rectangle(2, 2);
+		Shape shapeCircle = new Circle(3, 3);
+		
+		double perimeter1 = shapeRectangle.perimeter();
+		double perimeter2 = shapeCircle.perimeter();
+		
+		System.out.println("Perimeter rectangle : " + perimeter1 + " Perimeter circle " + perimeter2);
+		
+		printPerimeter(shape);
+	}
 
+	
+	public static void printPerimeter(Shape shape) {
+		double perimeter = shape.perimeter();
+		System.out.println(" Shape perimeter " + perimeter);
+	}
 }
---
+++ projects/HardSkillSession19/Triangle.java
@@ -0,0 +1,23 @@
+package HardSkillSession19;
+
+public class Triangle extends Shape {
+	public Triangle(int x, int y) {
+		super(x, y);
+	}
+
+	@Override
+	public double area() {
+		
+		return (double) x * y / 2;
+	}
+
+	@Override
+	public double perimeter() {
+	
+		var a = Math.sqrt(x * x + y * y);
+		return x + y + a;
+	}
+	
+	
+
+}
